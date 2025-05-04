211853b9d6f29c6790e4e630afb133290739f0b6
Ashley <167535539+hello135790@users.noreply.github.com>
2025-05-04 05:53:49 +0000

    Final with test cases

--- AppointmentBook.java
+++ AppointmentBook.java
@@ -1,12 +1,16 @@
 public class AppointmentBook {
-    
+    private boolean[][] schedule;
+    public AppointmentBook(boolean[][] schedule) {
+        this.schedule = schedule;
+    }
     /**
      * Returns true if minute in period is available for an appointment and returns
      * false otherwise
      * Preconditions: 1 <= period <= 8; 0 <= minute <= 59
      */
     private boolean isMinuteFree(int period, int minute) {
-        /* implementation not shown */ }
+        return schedule[period - 1][minute];
+    }
 
     /**
      * Marks the block of minutes that starts at startMinute in period and
@@ -15,7 +19,10 @@ public class AppointmentBook {
      * 1 <= duration <= 60
      */
     private void reserveBlock(int period, int startMinute, int duration) {
-        /* implementation not shown */ }
+        for(int i=startMinute; i<startMinute+duration; i++) {
+            schedule[period-1][i] = false;
+        }
+    }
 
     /**
      * Searches for the first block of duration free minutes during period, as
@@ -26,7 +33,18 @@ public class AppointmentBook {
      * Preconditions: 1 <= period <= 8; 1 <= duration <= 60
      */
     public int findFreeBlock(int period, int duration) {
-        /* to be implemented in part (a) */ }
+        int block = 0;
+        for (int i=0; i<60; i++) {
+            if (isMinuteFree(period, i)) {
+                block++;
+                if (block == duration){
+                    return i - duration + 1;
+                }
+            }
+            else block = 0;
+        }
+        return -1;
+    }
 
     /**
      * Searches periods from startPeriod to endPeriod, inclusive, for a block
@@ -37,8 +55,19 @@ public class AppointmentBook {
      * Preconditions: 1 <= startPeriod <= endPeriod <= 8; 1 <= duration <= 60
      */
     public boolean makeAppointment(int startPeriod, int endPeriod, int duration){
-     /* to be implemented in part (b) */ }
+        for(int i=startPeriod; i<=endPeriod; i++){
+            int freeBlock = findFreeBlock(i, duration);
+            if (freeBlock > -1) {
+                reserveBlock(i, freeBlock, duration);
+                return true;
+            }
+        }
+        return false;
+    }
 
     // There may be instance variables, constructors, and methods that are not shown.
-
+    public void printPeriod(int period) {
+        for(int i=0; i<schedule[period-1].length; i++) 
+        System.out.println(i + " " + schedule[period-1][i]);
+    }
 }
